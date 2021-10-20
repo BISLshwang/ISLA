4a6d96bd46f3833d72b26204ab81d60716572c22
BISLshwang <45934196+BISLshwang@users.noreply.github.com>
2021-10-20 18:05:26 +0900

    Update comparing_with_expressions.py

--- src/comparing_with_expressions.py
+++ src/comparing_with_expressions.py
@@ -2,7 +2,6 @@ import pickle
 import numpy as np
 import pandas as pd
 import math
-import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from collections import Counter
 from lifelines import KaplanMeierFitter
